469c819ad1eb47531d91ad57d085af8dd8cf5d7c
jmir1 <43830312+jmir1@users.noreply.github.com>
2021-01-13 14:30:17 +0100

    Update preview.js
    nothing of note

--- src/js/preview.js
+++ src/js/preview.js
@@ -1,5 +1,4 @@
-function Preview(dest)
-{
+function Preview(dest) {
     this.container = dest;
 
     this.screen = document.createElement('canvas');
@@ -11,10 +10,8 @@ function Preview(dest)
     var self = this;
     this.background = new Image();
     this.background.setAttribute('crossOrigin', 'anonymous');
-    this.background.addEventListener('load', function()
-    {
-        if (!/^http/i.test(this.src))
-        {
+    this.background.addEventListener('load', function () {
+        if (!/^http/i.test(this.src)) {
             return;
         }
 
@@ -31,13 +28,11 @@ function Preview(dest)
         ctx.fillStyle = 'rgba(0, 0, 0, .4)';
         ctx.fillRect(0, 0, self.screen.width, self.screen.height);
 
-        if (typeof self.beatmap.processBG != 'undefined')
-        {
+        if (typeof self.beatmap.processBG != 'undefined') {
             self.beatmap.processBG(ctx);
         }
 
-        canvas.toBlob(function(blob)
-        {
+        canvas.toBlob(function (blob) {
             var url = URL.createObjectURL(blob);
             self.background.src = url;
             self.container.style.backgroundImage = 'url(' + url + ')';
@@ -45,41 +40,32 @@ function Preview(dest)
             // URL.revokeObjectURL(url);
         });
     });
-    this.background.addEventListener('error', function()
-    {
+    this.background.addEventListener('error', function () {
         self.container.style.backgroundImage = 'none';
     });
 }
-Preview.prototype.load = function(beatmapID, bgURL, osuURL, success, fail)
-{
-    if (typeof this.xhr != 'undefined')
-    {
+Preview.prototype.load = function (beatmapID, bgURL, osuURL, success, fail) {
+    if (typeof this.xhr != 'undefined') {
         this.xhr.abort();
     }
 
     var self = this;
     this.xhr = new XMLHttpRequest();
-    this.xhr.addEventListener('load', function()
-    {
-        try
-        {
+    this.xhr.addEventListener('load', function () {
+        try {
             self.beatmap = Beatmap.parse(this.responseText);
-            //console.log(bgURL);
             self.background.src = bgURL;
             self.ctx.restore();
             self.ctx.save();
             self.beatmap.update(self.ctx);
             self.at(0);
 
-            if (typeof success == 'function')
-            {
+            if (typeof success == 'function') {
                 success.call(self);
             }
         }
-        catch (e)
-        {
-            if (typeof fail == 'function')
-            {
+        catch (e) {
+            if (typeof fail == 'function') {
                 fail.call(self, e);
             }
         }
@@ -87,8 +73,7 @@ Preview.prototype.load = function(beatmapID, bgURL, osuURL, success, fail)
     self.xhr.open('GET', osuURL);
     self.xhr.send();
 };
-Preview.prototype.at = function(time)
-{
+Preview.prototype.at = function (time) {
     this.ctx.save();
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.clearRect(0, 0, Beatmap.WIDTH, Beatmap.HEIGHT);
