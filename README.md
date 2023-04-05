c9cdcb4f54ebbedce69d7fbefa29403d03328cdb
Exsper <starlightexsper@foxmail.com>
2023-04-05 15:23:49 +0800

    change beatmap download source to   sayobot for better connection.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A simple osu! beatmap player  
 https://jmir.xyz/osu/preview.html#BEATMAP_ID  
 https://osu-preview-eight.vercel.app/preview.html#BEATMAP_ID
+
+change beatmap download source to sayobot for better connection.
--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,30 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano'),
     strip = require('gulp-strip-comments');
 
-gulp.task('default', function() {
-    return gulp.src('src/preview.html')
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
         .pipe(usemin({
-            inlinejs: [ uglify() ],
-            inlinecss: [ cssnano() ]
+            inlinejs: [uglify()],
+            inlinecss: [cssnano()]
         }))
         .pipe(strip())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('doc/'));
+});
+
+
+gulp.task('js', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('doc'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('src/**/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('doc'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/**/*.svg')
+        .pipe(gulp.dest('doc'));
 });
