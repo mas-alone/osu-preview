4ced1c7b7f4110ec01b0059cfbc5b562e6cf0c88
A M D <52590027+mas-alone@users.noreply.github.com>
2024-04-14 17:47:35 +0800

    Update catch.js

--- docs/js/catch/catch.js
+++ docs/js/catch/catch.js
@@ -1,1 +1,211 @@
-function Catch(t){Beatmap.call(this,t);for(var t=window.localStorage.getItem("DefaultColor"),t=(this.useDefaultColor=t?parseInt(t):0,window.localStorage.getItem("ColorChange")),e=(this.colorChange=t?parseInt(t):0,this.Colors.length&&!this.useDefaultColor?this.Colors.push(this.Colors.shift()):this.Colors=Catch.DEFAULT_COLORS,this.circleRadius=this.circleDiameter/2-4,this.smallRadius=this.circleRadius/2,this.tinyRadius=this.smallRadius/2,this.bananaRadius=.8*this.circleRadius,this.CATCHER_HEIGHT=Beatmap.HEIGHT/8,this.FALLOUT_TIME=this.CATCHER_HEIGHT/Beatmap.HEIGHT*this.approachTime,1),i=-1,a=1,h=0;h<this.HitObjects.length;h++){var s=this.HitObjects[h];s instanceof BananaShower?a=1:(s.newCombo||a)&&(i=(i+(e=1)+s.comboSkip)%this.Colors.length,a=0),s.combo=e++,s.color=this.colorChange?this.Colors[h%this.Colors.length]:this.Colors[i],(s instanceof JuiceStream||s instanceof BananaShower)&&s.buildNested()}this.CATCHER_BASE_SIZE=106.75,this.ALLOWED_CATCH_RANGE=.8,this.HYPER_DASH_TRANSITION_DURATION=180,this.calculateScale=1-.7*(this.CircleSize-5)/5,this.catchWidth=this.CATCHER_BASE_SIZE*Math.abs(this.calculateScale)*this.ALLOWED_CATCH_RANGE,this.halfCatcherWidth=this.catchWidth/2,this.halfCatcherWidth/=this.ALLOWED_CATCH_RANGE,this.BASE_DASH_SPEED=1,this.BASE_WALK_SPEED=.5,this.RNG_SEED=1337;for(var c=new LegacyRandom(this.RNG_SEED),h=0;h<this.HitObjects.length;h++){var l=this.HitObjects[h];l instanceof BananaShower?l.nested.forEach(t=>{t.x+=c.NextDouble()*Beatmap.MAX_X,c.Next(),c.Next(),c.Next()}):l instanceof JuiceStream&&l.nested.forEach(t=>{"TinyDroplet"===t.type?t.x+=Math.clamp(c.Next(-20,20),-t.x,Beatmap.MAX_X-t.x):"Droplet"===t.type&&c.Next()})}this.palpableObjects=[],this.fullCatchObjects=[];for(h=0;h<this.HitObjects.length;h++){var o,r=this.HitObjects[h];r instanceof Fruit?(o=new PalpableCatchHitObject({type:"Fruit",time:r.time,x:r.position.x,color:r.color,radius:this.circleRadius},this),this.palpableObjects.push(o),this.fullCatchObjects.push(o)):r instanceof BananaShower?r.nested.forEach(t=>{this.fullCatchObjects.push(t)}):r instanceof JuiceStream&&r.nested.forEach(t=>{this.fullCatchObjects.push(t),"TinyDroplet"!=t.type&&this.palpableObjects.push(t)})}this.palpableObjects.sort((t,e)=>t.time-e.time),this.fullCatchObjects.sort((t,e)=>t.time-e.time);let p=0,n=this.halfCatcherWidth;for(let t=0;t<this.palpableObjects.length-1;t++){var C=this.palpableObjects[t],f=this.palpableObjects[t+1],m=(C.hyperDash=!1,f.x>C.x?1:-1),E=f.time-C.time-1e3/60/4,f=Math.abs(f.x-C.x)-(p==m?n:this.halfCatcherWidth),E=E*this.BASE_DASH_SPEED-f;n=E<0?(C.hyperDash=!0,this.halfCatcherWidth):Math.clamp(E,0,this.halfCatcherWidth),p=m}}Catch.prototype=Object.create(Beatmap.prototype,{approachTime:{get:function(){return this.ApproachRate<5?1800-120*this.ApproachRate:1200-150*(this.ApproachRate-5)}},circleDiameter:{get:function(){return 108.848-8.9646*this.CircleSize}}}),(Catch.prototype.constructor=Catch).prototype.hitObjectTypes={},Catch.ID=2,(Beatmap.modes[Catch.ID]=Catch).DEFAULT_COLORS=["rgb(255,210,128)","rgb(128,255,128)","rgb(128,191,255)","rgb(191,128,255)"],Catch.prototype.update=function(t){t.translate((Beatmap.WIDTH-Beatmap.MAX_X)/2,(Beatmap.HEIGHT-Beatmap.MAX_Y)/2)},Catch.prototype.draw=function(t,e){for(void 0===this.tmp.first&&(this.tmp.first=0,this.tmp.last=-1);this.tmp.first<this.fullCatchObjects.length;){if(t<=(i=this.fullCatchObjects[this.tmp.first]).time+this.FALLOUT_TIME)break;this.tmp.first++}for(;this.tmp.last+1<this.fullCatchObjects.length&&t>=this.fullCatchObjects[this.tmp.last+1].time-1.1*this.approachTime;)this.tmp.last++;for(var i,a=this.tmp.last;a>=this.tmp.first;a--)t>(i=this.fullCatchObjects[a]).time+this.FALLOUT_TIME||i.draw(t,e)},Catch.prototype.processBG=function(t){t.beginPath(),t.moveTo(0,Beatmap.HEIGHT-this.CATCHER_HEIGHT),t.lineTo(Beatmap.WIDTH,Beatmap.HEIGHT-this.CATCHER_HEIGHT),t.strokeStyle="#fff",t.lineWidth=2,t.stroke();var e=this.catchWidth/60;t.beginPath(),t.rect(Beatmap.WIDTH/2-this.catchWidth/2,Beatmap.HEIGHT-this.CATCHER_HEIGHT-e/2,this.catchWidth,e),t.strokeStyle="#fff",t.lineWidth=8,t.stroke(),t.fillStyle="#fff",t.fill()};
+function Catch(osu) {
+    Beatmap.call(this, osu);
+
+    let savedDefaultColor = window.localStorage.getItem("DefaultColor");
+    this.useDefaultColor = (savedDefaultColor) ? parseInt(savedDefaultColor) : 0;
+
+    let savedColorChange = window.localStorage.getItem("ColorChange");
+    this.colorChange = (savedColorChange) ? parseInt(savedColorChange) : 0;
+
+    if (this.Colors.length && !this.useDefaultColor) {
+        this.Colors.push(this.Colors.shift());
+    }
+    else {
+        this.Colors = Catch.DEFAULT_COLORS;
+    }
+
+    this.circleRadius = this.circleDiameter / 2 - 4;
+    this.smallRadius = this.circleRadius / 2;
+    this.tinyRadius = this.smallRadius / 2;
+    this.bananaRadius = this.circleRadius * 0.8;
+
+    this.CATCHER_HEIGHT = Beatmap.HEIGHT / 8;
+    this.FALLOUT_TIME = (this.CATCHER_HEIGHT / Beatmap.HEIGHT) * this.approachTime;
+
+    var combo = 1,
+        comboIndex = -1,
+        setComboIndex = 1;
+    for (var i = 0; i < this.HitObjects.length; i++) {
+        let hitObject = this.HitObjects[i];
+        if (hitObject instanceof BananaShower) {
+            setComboIndex = 1;
+        }
+        else if (hitObject.newCombo || setComboIndex) {
+            combo = 1;
+            comboIndex = ((comboIndex + 1) + hitObject.comboSkip) % this.Colors.length;
+            setComboIndex = 0;
+        }
+        hitObject.combo = combo++;
+        hitObject.color = (this.colorChange) ? this.Colors[i % this.Colors.length] : this.Colors[comboIndex];
+
+        if (hitObject instanceof JuiceStream || hitObject instanceof BananaShower) {
+            hitObject.buildNested();
+        }
+    }
+
+    this.CATCHER_BASE_SIZE = 106.75;
+    this.ALLOWED_CATCH_RANGE = 0.8;
+    this.HYPER_DASH_TRANSITION_DURATION = 180;
+    this.calculateScale = 1.0 - 0.7 * (this.CircleSize - 5) / 5;
+    this.catchWidth = this.CATCHER_BASE_SIZE * Math.abs(this.calculateScale) * this.ALLOWED_CATCH_RANGE;
+    this.halfCatcherWidth = this.catchWidth / 2;
+    this.halfCatcherWidth /= this.ALLOWED_CATCH_RANGE;
+    this.BASE_DASH_SPEED = 1;
+    this.BASE_WALK_SPEED = 0.5;
+
+    // sliders & spins xoffset
+    this.RNG_SEED = 1337;
+    var rng = new LegacyRandom(this.RNG_SEED);
+
+    for (var i = 0; i < this.HitObjects.length; i++) {
+        let hitObject = this.HitObjects[i];
+        // console.log(hitObject.nested)
+        if (hitObject instanceof BananaShower) {
+            hitObject.nested.forEach(banana => {
+                banana.x += (rng.NextDouble() * Beatmap.MAX_X);
+                rng.Next(); // osu!stable retrieved a random banana type
+                rng.Next(); // osu!stable retrieved a random banana rotation
+                rng.Next(); // osu!stable retrieved a random banana colour
+            });
+        }
+        else if (hitObject instanceof JuiceStream) {
+            hitObject.nested.forEach(item => {
+                if (item.type === "TinyDroplet") item.x += Math.clamp(rng.Next(-20, 20), -item.x, Beatmap.MAX_X - item.x);
+                else if (item.type === "Droplet") rng.Next(); // osu!stable retrieved a random droplet rotation
+            });
+        }
+    }
+
+    // catch objects
+    this.palpableObjects = [];
+    this.fullCatchObjects = [];
+    for (var i = 0; i < this.HitObjects.length; i++) {
+        let hitObject = this.HitObjects[i];
+        if (hitObject instanceof Fruit) {
+            let pch = new PalpableCatchHitObject({
+                type: "Fruit",
+                time: hitObject.time,
+                x: hitObject.position.x,
+                color: hitObject.color,
+                radius: this.circleRadius,
+            }, this);
+
+            this.palpableObjects.push(pch);
+            this.fullCatchObjects.push(pch);
+        }
+        else if (hitObject instanceof BananaShower) {
+            hitObject.nested.forEach(banana => {
+                this.fullCatchObjects.push(banana);
+            });
+        }
+        else if (hitObject instanceof JuiceStream) {
+            hitObject.nested.forEach(item => {
+                this.fullCatchObjects.push(item);
+                if (item.type != "TinyDroplet") this.palpableObjects.push(item);
+            });
+        }
+    }
+
+    this.palpableObjects.sort((a, b) => a.time - b.time);
+    this.fullCatchObjects.sort((a, b) => a.time - b.time);
+
+    // hyperdash
+    let lastDirection = 0;
+    let lastExcess = this.halfCatcherWidth;
+
+    for (let i = 0; i < this.palpableObjects.length - 1; i++) {
+        var currentObject = this.palpableObjects[i];
+        var nextObject = this.palpableObjects[i + 1];
+
+        currentObject.hyperDash = false;
+
+        let thisDirection = nextObject.x > currentObject.x ? 1 : -1;
+        let timeToNext = nextObject.time - currentObject.time - 1000 / 60 / 4; // 1/4th of a frame of grace time, taken from osu-stable
+        let distanceToNext = Math.abs(nextObject.x - currentObject.x) - (lastDirection == thisDirection ? lastExcess : this.halfCatcherWidth);
+        let distanceToHyper = timeToNext * this.BASE_DASH_SPEED - distanceToNext;
+
+        if (distanceToHyper < 0) {
+            currentObject.hyperDash = true;
+            lastExcess = this.halfCatcherWidth;
+        }
+        else {
+            lastExcess = Math.clamp(distanceToHyper, 0, this.halfCatcherWidth);
+        }
+
+        lastDirection = thisDirection;
+    }
+
+}
+Catch.prototype = Object.create(Beatmap.prototype, {
+    approachTime: { // droptime
+        get: function () {
+            return this.ApproachRate < 5
+                ? 1800 - this.ApproachRate * 120
+                : 1200 - (this.ApproachRate - 5) * 150;
+        }
+    },
+    // https://github.com/itdelatrisu/opsu/commit/8892973d98e04ebaa6656fe2a23749e61a122705
+    circleDiameter: {
+        get: function () {
+            return 108.848 - this.CircleSize * 8.9646;
+        }
+    }
+});
+Catch.prototype.constructor = Catch;
+Catch.prototype.hitObjectTypes = {};
+Catch.ID = 2;
+Beatmap.modes[Catch.ID] = Catch;
+Catch.DEFAULT_COLORS = [
+    'rgb(255,210,128)',
+    'rgb(128,255,128)',
+    'rgb(128,191,255)',
+    'rgb(191,128,255)'
+];
+Catch.prototype.update = function (ctx) {
+    ctx.translate((Beatmap.WIDTH - Beatmap.MAX_X) / 2, (Beatmap.HEIGHT - Beatmap.MAX_Y) / 2);
+};
+Catch.prototype.draw = function (time, ctx) {
+    if (typeof this.tmp.first == 'undefined') {
+        this.tmp.first = 0;
+        this.tmp.last = -1;
+    }
+
+    while (this.tmp.first < this.fullCatchObjects.length) {
+        var catchHitObject = this.fullCatchObjects[this.tmp.first];
+        if (time <= catchHitObject.time + this.FALLOUT_TIME) {
+            break;
+        }
+        this.tmp.first++;
+    }
+    while (this.tmp.last + 1 < this.fullCatchObjects.length &&
+        time >= this.fullCatchObjects[this.tmp.last + 1].time - this.approachTime * 1.1) {
+        this.tmp.last++;
+    }
+    for (var i = this.tmp.last; i >= this.tmp.first; i--) {
+        var catchHitObject = this.fullCatchObjects[i];
+        if (time > catchHitObject.time + this.FALLOUT_TIME) {
+            continue;
+        }
+        catchHitObject.draw(time, ctx);
+    }
+};
+Catch.prototype.processBG = function (ctx) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // line
+    ctx.beginPath();
+    ctx.moveTo(0, Beatmap.HEIGHT - this.CATCHER_HEIGHT);
+    ctx.lineTo(Beatmap.WIDTH, Beatmap.HEIGHT - this.CATCHER_HEIGHT);
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    // plate
+    let plateHeight = this.catchWidth / 60;
+    ctx.beginPath();
+    ctx.rect(Beatmap.WIDTH / 2 - this.catchWidth / 2, Beatmap.HEIGHT - this.CATCHER_HEIGHT - plateHeight / 2, this.catchWidth, plateHeight);
+    ctx.strokeStyle = '#fff';
+    ctx.lineWidth = 8;
+    ctx.stroke();
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+};
